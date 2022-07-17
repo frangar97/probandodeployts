5808199e2908054c86cd83453a19debb3296156d
Francisco <frangarcia38@hotmail.com>
2022-07-17 15:11:46 -0600

    Test

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express from 'express';
+import { Pool } from 'pg';
+import { PORT, DB_CONNECTION } from './config';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectionString: DB_CONNECTION
+});
+
+app.get("/", async (req, res) => {
+    const response = await pool.query("SELECT * FROM Usuario");
+    res.json(response.rows);
+});
+
+app.listen(PORT, () => console.log(`Servidor iniciado en puerto ${PORT}`))
